73d09415a579b48547e1471bbb8e77a9cc6526a5
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-07-06 01:27:32 +0100

    Add default XCode project

---
+++ XCodeHaskell/XCodeHaskell/XCodeHaskellApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeHaskellApp.swift
+//  XCodeHaskell
+//
+//  Created by Rodrigo Mesquita on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct XCodeHaskellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
