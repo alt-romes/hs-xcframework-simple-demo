dcb82ebc3bdaf7484d3eb67008b616171c103433
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-07-06 01:29:22 +0100

    Init/Exit RTS

--- XCodeHaskell/XCodeHaskell/XCodeHaskellApp.swift
+++ XCodeHaskell/XCodeHaskell/XCodeHaskellApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Haskell.Foreign.Rts
 
 @main
 struct XCodeHaskellApp: App {
+    init() {
+        hs_init(nil, nil)
+
+        NotificationCenter.default
+          .addObserver(forName: NSApplication.willTerminateNotification,
+                       object: nil, queue: .main) { _ in
+          hs_exit()
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
